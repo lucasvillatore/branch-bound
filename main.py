b86b1793242ee9c3651ce6998b35dfd49c2c05ef
Lucas Block <lucas.blockv@gmail.com>
2021-07-17 15:34:17 -0300

    add input + leitura grafo

--- main.py
+++ main.py
@@ -4,22 +4,37 @@ def splita_entrada_em_inteiros(entrada):
     quantidade = map(int, entrada.split())
     quantidade_em_inteiros = list(quantidade) 
 
+    quantidade_em_inteiros.insert(0, 0) 
+    
     return quantidade_em_inteiros
 
-matriz = []
+def deixa_lista_mesmo_tamanho(index, quantidade):
+    for i in range(0, index):
+        quantidade.insert(0, 0)    
+
+    return quantidade
+
+def existe_caminho_para_inicio(indice, grafo):
+    if grafo[indice][0] > 0:
+        return True
+    return False
+
+def pega_grafo_input(numero_vertices):
+    grafo = []
+    for index in range(0, numero_vertices - 1):
+        quantidade = deixa_lista_mesmo_tamanho(index, splita_entrada_em_inteiros(input()))
+        grafo.append(quantidade)           
+    
+    ultimo_elemento = [0 for i in range(0, numero_vertices)]
+    grafo.append(ultimo_elemento)
+
+    for i in range(0, numero_vertices):
+        for j in range(0, i):
+            grafo[i][j] = grafo[j][i]
+    
+    return grafo
+
+
 if __name__ == '__main__':
     numero_vertices = int(input())
-    for index in range(0, numero_vertices - 1):
-        quantidade = splita_entrada_em_inteiros(input())
-        for i in range(0, index):
-            quantidade.insert(0, -1)     
-        matriz.append(quantidade)
-
-
-    for i in range(numero_vertices - 1):
-        for j in range(numero_vertices - 1):
-            # if (matriz[i][j] == -1):
-                # print("  ",end="")
-            # else:
-            print(f"{matriz[i][j]} ", end="")
-        print("")
+    grafo = pega_grafo_input(numero_vertices)
